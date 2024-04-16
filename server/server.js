9ee5a9975c0c062726d45c9c8d8112e0b430e46c
Thembinkosi Mahlobo <themba.romano@hotmail.co.uk>
2024-04-16 13:37:37 +0100

    update changes made to server

--- server/server.js
+++ server/server.js
@@ -1,38 +1,34 @@
-import Database from "better-sqlite3";
 import express, { request, response } from "express";
 import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-const db = new Database("database.db");
 
-app.get("/...", function (request, response) {
+app.get("/", function (request, response) {
   response.json("This is root route");
 });
-app.get("/", function (request, response) {
-  constant = db.prepare("SELECT* FROM ...").all();
-  response.json();
-  console.log(...);
-});
-app.listen(..., function() {
-console.log("server is running on port...")
+
+app.listen(8080, function () {
+  console.log("server is running on port 8080");
 });
 
-let ... = [];
-app.get("/", (request, response) => {
-    response.json(...);
+app.post("/add/user", (request, response) => {
+  console.log(request.body);
+  response.json(request.body);
 });
 
-app.post("/",(request. response) = >{
-    const {...Database, ...} = request.body; timetstamp: newDate () };
-    const new...{...Database, ...Database.SqliteError,}
-    ...Database.push(new...);
-    response.status().json({... ""});
+app.get("/all/users", (request, response) => {
+  response.json(request.body);
+  console.log(request.body);
 });
 
-// consta timestamp = Date.now();
-//console.log(timestamp)
-//const timestampInSeconds= Math.floor(Date.now() / 1000);
-//console.log(timestampInSeconds)
+/*app.post("/tvshow", (request, response) => {
+    console.log(request.body);
+    newData = request.body.tvshow;
+    console.log(newData);
+    response.json(request.body);
+  });*/
 
+// app.delete()
+//app.put()
