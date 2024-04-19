6364846ca59a580ac7f931f68cfb55086cc68123
Kevin <kevin.barr88@gmail.com>
2024-04-19 11:51:35 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,6 +27,15 @@ app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 
 app.listen(8080, function () {
